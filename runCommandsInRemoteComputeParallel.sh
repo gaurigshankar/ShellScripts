e8ec3e8ab3e0ec991f62a83c9e459bb424f25165
Gauri Shankar P <gauri.gshankar@yahoo.co.in>
2020-09-18 10:15:23 -0700

    Update runCommandsInRemoteComputeParallel.sh

--- runCommandsInRemoteComputeParallel.sh
+++ runCommandsInRemoteComputeParallel.sh
@@ -3,3 +3,5 @@
 #### have IPs in a single file
 
 pssh -l app -h ips.txt -i 'pgrep -fl node | wc -l'
+
+pssh  -H "10.49.140.61 109.49.9.102 107.81.136.118 110.46.10.90"  -l gauri -x '-i /Users/home/.ssh/remote_rsa' -P "cat /usr/home/index.js | grep log"
